26267b2aecf515f64ad693a2ddc251417aa1ba1f
Ahmed Siah <ahmed.siah@cahs-bc.ca>
2020-06-23 14:40:16 -0700

    Ahmed add one line

--- src/script.R
+++ src/script.R
@@ -1,1 +1,2 @@
 ## This is my super great R script
+I like the git lesson and your fancy layout EMACS - I would like to use similar layout if ou have some tutorials or packages o try - Many Thanks for all your courses -
